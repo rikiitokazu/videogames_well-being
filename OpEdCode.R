ea6898cba67f9e9bf36b6e2be560f7eec59a61d2
Riki Itokazu <itokazuriki@gmail.com>
2025-05-06 16:54:25 -0400

    r code

--- OpEdCode.R
+++ OpEdCode.R
@@ -1,20 +1,7 @@
----
-title: "Data-Driven Op-Ed"
-author: "Riki Itokazu"
-date: "2025-05-04"
-output: html_document
----
-
-# Videogame Usage vs. Psychological Well-Being
-
-### Setup environment
-```{r}
-gaming <- read.csv("gamingstudy.csv")
-```
-
-### Look at distributions of GAD_T, SPIN_T, SWL_T
-```{r}
 library(ggplot2)
+
+gaming <- read.csv("gamingstudy.csv")
+
 
 # Removing outliers of hours played per week (considering quartile 25%-75% of data)
 Q1 <- quantile(gaming$Hours, 0.25, na.rm = TRUE)
@@ -50,11 +37,10 @@ ggplot(gaming, aes(x = Hours)) +
   geom_histogram(binwidth = 1, alpha = 0.7, position = "dodge", color = "black") +
   labs(title = "Histogram of Hours Played Per Week", x = "Hours Per Week", y = "Frequency") +
   theme_minimal()
-```
+
 ### Some metadata and charts
-```{r}
 gaming$levels <- ifelse(gaming$Hours < 10, "Less than 10", 
-                              ifelse(gaming$Hours <= 20, "10-20", "More than 20"))
+                        ifelse(gaming$Hours <= 20, "10-20", "More than 20"))
 
 # Count the number of occurrences for each level
 levels_counts <- as.data.frame(table(gaming$levels))
@@ -83,10 +69,7 @@ levels_counts_under25$percentage <- levels_counts_under25$count / sum(levels_cou
 print(levels_counts_under25)
 
 
-```
-
 ### T-test for all individuals that play more than 20 hours
-```{r}
 # hours_mean ~ 20 hours per week
 hours_mean <- mean(gaming$Hours, na.rm = TRUE)
 print(hours_mean)
@@ -99,57 +82,48 @@ t_test_gad_hours_group <- t.test(GAD_T ~ hours_threshold, data = gaming, na.rm =
 print(t_test_gad_hours_group)
 
 
-```
 
 ### T-test for employed individuals or students that play between 3 and 15 hours
-```{r}
 gaming_employed = gaming[gaming$Work == "Employed" | gaming$Work == "Student at college / university", ]
 
 # hours for employed ~20 hours
 new_hours_mean <- mean(gaming_employed$Hours, na.rm = TRUE)
 
 gaming_employed$levels <- ifelse(gaming_employed$Hours < 3, "Little to None", 
-                              ifelse(gaming_employed$Hours <= 15, "Sometimes", "More than 15 hours"))
+                                 ifelse(gaming_employed$Hours <= 15, "Sometimes", "More than 15 hours"))
 
 subset_two_groups <- gaming_employed[gaming_employed$levels %in% c("Little to None", "Sometimes"), ]
 
 t_test_result <- t.test(GAD_T ~ levels, data = subset_two_groups)
 print(t_test_result)
-```
+
 ### T-test for employed individuals or students that play more than 15 hours
-```{r}
 subset_two_groups <- gaming_employed[gaming_employed$levels %in% c("Sometimes", "More than 15 hours"), ]
 
 t_test_result <- t.test(GAD_T ~ levels, data = subset_two_groups)
 print(t_test_result)
-```
+
 
 ### SPIN (social phobia inventory score) for those who play 3-15 hours (sometimes)
-```{r}
 subset_two_groups <- gaming_employed[gaming_employed$levels %in% c("Little to None", "Sometimes"), ]
 
 t_test_result <- t.test(SPIN_T ~ levels, data = subset_two_groups)
 print(t_test_result)
-```
+
 ### A linear regression model on SWL_T
-```{r}
 model <- lm(SWL_T ~ Hours + Degree + Work, data = gaming)
 summary(model)
-```
+
 
 
 ### Box plot on GAD_T
-```{r}
 ggplot(gaming_employed[!is.na(gaming_employed$levels), ], aes(x = levels, y = GAD_T)) +
   geom_boxplot(fill = "lightblue") +
   labs(title = "Anxiety (GAD_T) Across Gaming Frequency (Employed or Students)",
        x = "Gaming Frequency", y = "GAD_T Score")
 
 
-```
-
 ### More visualizations, focusing on SWL_T (Satisfaction with life)
-```{r}
 # By their work status
 mean_swl_work <- aggregate(SWL_T ~ Work, data = gaming, FUN = function(x) mean(x, na.rm = TRUE))
 
@@ -166,7 +140,6 @@ ggplot(mean_swl_levels, aes(x = levels, y = SWL_T, fill = levels)) +
   geom_bar(stat = "identity") +
   labs(title = "SWL_T by Gaming Frequency", x = "Gaming Level", y = "Mean SWL_T") +
   theme_minimal()
-```
-```
+
 
 
